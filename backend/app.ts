ce7f6305db30b4d46a6b6a000942280f3d025f29
Schuyler deVos <schuylerdevos@gmail.com>
2023-12-24 15:52:22 -0500

    add firestore for persistence layer

--- backend/app.ts
+++ backend/app.ts
@@ -1,19 +1,32 @@
-import express, {Request, Response} from 'express';
-import {getMachineHealth} from './machineHealth';
+import express, { Request, Response } from "express";
+import { getMachineHealth } from "./machineHealth";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+const serviceAccount = require("./engineering-challenge-409002-98ed1d4b8489.json");
 
 const app = express();
 const port = 3001;
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+const machineDb = db.collection("machineScores");
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Endpoint to get machine health score
-app.post('/machine-health', (req: Request, res: Response) => {
+app.post("/machine-health", (req: Request, res: Response) => {
   const result = getMachineHealth(req);
   if (result.error) {
     res.status(400).json(result);
   } else {
     res.json(result);
+    machineDb
+      .doc(Date.now().toString())
+      .set({ ...result, user: req.body.user });
   }
 });
 
